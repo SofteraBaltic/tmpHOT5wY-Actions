688c29955326931ef8c1ebbde2aafeed00577b72
SofteraBaltic <SofteraBaltic@users.noreply.github.com>
2024-01-24 08:51:11 +0000

    Deploying AL-Go from main (bf1e8f35935cb9a2361a40e47b6bf3a2ec0dad9c) to main

---
+++ IncrementVersionNumber/IncrementVersionNumber.ps1
@@ -0,0 +1,115 @@
+Param(
+    [Parameter(HelpMessage = "The GitHub actor running the action", Mandatory = $false)]
+    [string] $actor,
+    [Parameter(HelpMessage = "The GitHub token running the action", Mandatory = $false)]
+    [string] $token,
+    [Parameter(HelpMessage = "Specifies the parent telemetry scope for the telemetry signal", Mandatory = $false)]
+    [string] $parentTelemetryScopeJson = '7b7d',
+    [Parameter(HelpMessage = "List of project names if the repository is setup for multiple projects (* for all projects)", Mandatory = $false)]
+    [string] $projects = '*',
+    [Parameter(HelpMessage = "Updated Version Number. Use Major.Minor for absolute change, use +Major.Minor for incremental change.", Mandatory = $true)]
+    [string] $versionNumber,
+    [Parameter(HelpMessage = "Set the branch to update", Mandatory = $false)]
+    [string] $updateBranch,
+    [Parameter(HelpMessage = "Direct commit?", Mandatory = $false)]
+    [bool] $directCommit
+)
+
+$telemetryScope = $null
+
+try {
+    . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+    $serverUrl, $branch = CloneIntoNewFolder -actor $actor -token $token -updateBranch $updateBranch -DirectCommit $directCommit -newBranchPrefix 'increment-version-number'
+    $baseFolder = (Get-Location).path
+    DownloadAndImportBcContainerHelper -baseFolder $baseFolder
+
+    import-module (Join-Path -path $PSScriptRoot -ChildPath "..\TelemetryHelper.psm1" -Resolve)
+    $telemetryScope = CreateScope -eventId 'DO0076' -parentTelemetryScopeJson $parentTelemetryScopeJson
+
+    $addToVersionNumber = "$versionNumber".StartsWith('+')
+    if ($addToVersionNumber) {
+        $versionNumber = $versionNumber.Substring(1)
+    }
+    try {
+        $newVersion = [System.Version]"$($versionNumber).0.0"
+    }
+    catch {
+        throw "Version number ($versionNumber) is malformed. A version number must be structured as <Major>.<Minor> or +<Major>.<Minor>"
+    }
+
+    $settings = $env:Settings | ConvertFrom-Json
+    $projectList = @(GetProjectsFromRepository -baseFolder $baseFolder -projectsFromSettings $settings.projects -selectProjects $projects)
+    foreach($project in $projectList) {
+        try {
+            Write-Host "Reading settings from $project\$ALGoSettingsFile"
+            $settingsJson = Get-Content "$project\$ALGoSettingsFile" -Encoding UTF8 | ConvertFrom-Json
+            if ($settingsJson.PSObject.Properties.Name -eq "repoVersion") {
+                $oldVersion = [System.Version]"$($settingsJson.repoVersion).0.0"
+                if ((!$addToVersionNumber) -and $newVersion -le $oldVersion) {
+                    throw "The new version number ($($newVersion.Major).$($newVersion.Minor)) must be larger than the old version number ($($oldVersion.Major).$($oldVersion.Minor))"
+                }
+                $repoVersion = $newVersion
+                if ($addToVersionNumber) {
+                    $repoVersion = [System.Version]"$($newVersion.Major+$oldVersion.Major).$($newVersion.Minor+$oldVersion.Minor).0.0"
+                }
+                $settingsJson.repoVersion = "$($repoVersion.Major).$($repoVersion.Minor)"
+            }
+            else {
+                $repoVersion = $newVersion
+                if ($addToVersionNumber) {
+                    $repoVersion = [System.Version]"$($newVersion.Major+1).$($newVersion.Minor).0.0"
+                }
+                Add-Member -InputObject $settingsJson -NotePropertyName "repoVersion" -NotePropertyValue "$($repoVersion.Major).$($repoVersion.Minor)" | Out-Null
+            }
+            $useRepoVersion = (($settingsJson.PSObject.Properties.Name -eq "versioningStrategy") -and (($settingsJson.versioningStrategy -band 16) -eq 16))
+            $settingsJson
+            $settingsJson | Set-JsonContentLF -path "$project\$ALGoSettingsFile"
+        }
+        catch {
+            throw "Settings file $project\$ALGoSettingsFile is malformed.$([environment]::Newline) $($_.Exception.Message)."
+        }
+
+        $folders = @('appFolders', 'testFolders' | ForEach-Object { if ($SettingsJson.PSObject.Properties.Name -eq $_) { $settingsJson."$_" } })
+        if (-not ($folders)) {
+            $folders = Get-ChildItem -Path $project | Where-Object { $_.PSIsContainer -and (Test-Path (Join-Path $_.FullName 'app.json')) } | ForEach-Object { $_.Name }
+        }
+        $folders | ForEach-Object {
+            Write-Host "Modifying app.json in folder $project\$_"
+            $appJsonFile = Join-Path "$project\$_" "app.json"
+            if (Test-Path $appJsonFile) {
+                try {
+                    $appJson = Get-Content $appJsonFile -Encoding UTF8 | ConvertFrom-Json
+                    $oldVersion = [System.Version]$appJson.Version
+                    if ($useRepoVersion) {
+                        $appVersion = $repoVersion
+                    }
+                    elseif ($addToVersionNumber) {
+                        $appVersion = [System.Version]"$($newVersion.Major+$oldVersion.Major).$($newVersion.Minor+$oldVersion.Minor).0.0"
+                    }
+                    else {
+                        $appVersion = $newVersion
+                    }
+                    $appJson.Version = "$appVersion"
+                    $appJson | Set-JsonContentLF -path $appJsonFile
+                }
+                catch {
+                    throw "Application manifest file($appJsonFile) is malformed."
+                }
+            }
+        }
+    }
+    if ($addToVersionNumber) {
+        CommitFromNewFolder -serverUrl $serverUrl -commitMessage "Increment Version number by $($newVersion.Major).$($newVersion.Minor)" -branch $branch | Out-Null
+    }
+    else {
+        CommitFromNewFolder -serverUrl $serverUrl -commitMessage "New Version number $($newVersion.Major).$($newVersion.Minor)" -branch $branch | Out-Null
+    }
+
+    TrackTrace -telemetryScope $telemetryScope
+}
+catch {
+    if (Get-Module BcContainerHelper) {
+        TrackException -telemetryScope $telemetryScope -errorRecord $_
+    }
+    throw
+}
